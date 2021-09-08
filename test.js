827036831432d11ab43c2f3addf1979de16f2f12
RiddlerX2 <timofeev.andreyx2@gmail.com>
2021-09-08 12:11:08 +0300

    1.0.3

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-import { StarlineAuth as slAuth } from './index';
-import { Starline, Beacons } from './index';
+let slAuth = require('./index').StarlineAuth;
+let slBase = require('./index');
 
 async function x() {
 	let objAuth = new slAuth(2791, 'GLJuE1WqIkbLnDHPXJQUN7MGzcaAs3c3', '100@dlx51.ru', 'Fusins123starline');
-	let objBase = new Starline(objAuth);
+	let objBase = new slBase.Starline(objAuth);
 	objBase.waitReady()
 	.then(async (data) => {
-		let objBeac = new Beacons(objAuth);
+		let objBeac = new slBase.Beacons(objAuth);
 		await objBeac.updateList();
 		console.log('Done');
 	})
